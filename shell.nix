14047a22177d9ada0d669156a5baea44b8877c5f
Joel Rivera <rivera@joel.mx>
2018-11-29 00:49:33 -0600

    Remove the explicit wheel dep for Nikola and _aesthetic_ changes in the shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,9 @@
 with (import <nixpkgs> {});
 let
   inherit(builtins) typeOf mapAttrs;
-  vanillaPython = python36;
+
+  vanillaPython = python36; # latest support version for Nikola
+
   # ghp_import2 is required to deploy directly from Nikola
   # and is not pre-packaged in nixpkgs
   ghp_import2 = with vanillaPython.pkgs;
@@ -14,6 +16,7 @@ let
          sha256 = "0zvdr67f1gr21ygnahmagfxbn1r1yb2gyiii9fb44nvcn935iygx";
       };
     };
+
   python = vanillaPython.override {
       packageOverrides = (self: super:
          (mapAttrs (name: value: (
@@ -21,10 +24,12 @@ let
          then
            if name == "Nikola" then
              (value.overridePythonAttrs (p: {
-                       propagatedBuildInputs = p.propagatedBuildInputs ++ [ super.wheel ghp_import2 ];
+                       # add the ghp-import2 dependency and specify that we are skipping
+                       # the tests for Nikola
+                       propagatedBuildInputs = p.propagatedBuildInputs ++ [ ghp_import2 ];
                        doInstallCheck = false;
                        doCheck = false; }))
-           else
+           else # don't run the test suite on every python dependency
              (value.overridePythonAttrs (p: {
                      doInstallCheck = false;
                      doCheck = false;
@@ -32,7 +37,7 @@ let
          else value))
            super)
       );
-    };
+  };
 in mkShell {
     buildInputs = [ python.pkgs.Nikola  ];
 }
