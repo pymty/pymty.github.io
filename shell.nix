e9a53d89da1a128689d4e05272b46a991bbd3fa6
Joel Rivera <rivera@joel.mx>
2018-11-29 00:43:57 -0600

    Fix the shell.nix script to use nix to setup nikola with the extra dependency to deploy to github

--- shell.nix
+++ shell.nix
@@ -1,15 +1,38 @@
-{ pkgs ? <nixpkgs> }:
-with(import pkgs {});
+with (import <nixpkgs> {});
 let
-  python = python37;
-  envPath = builtins.getEnv "PATH";
-in mkShell rec {
-  inputsFrom = [ python python.pkgs.virtualenv  ];
-  PATH = "${ lib.makeBinPath inputsFrom  }:${ envPath }";
-  # so we dont break the wheels.. 1980
-  SOURCE_DATE_EPOCH = "315532800";
-  # for _some reason_ the include of libxml is inside another sub-folder
-  CFLAGS = (map (p: "-I ${ p }/include") [ zlib.dev libxslt.dev python ]) ++ [ "-I ${ libxml2.dev }/include/libxml2" ];
-  LDFLAGS= (map (p: "-L${ p }/lib") [ python  libxml2.out libxslt.out zlib.out ]);
-  LD_LIBRARY_PATH=lib.makeLibraryPath [ python libxslt libxml2 libzip  zlib ];
+  inherit(builtins) typeOf mapAttrs;
+  vanillaPython = python36;
+  # ghp_import2 is required to deploy directly from Nikola
+  # and is not pre-packaged in nixpkgs
+  ghp_import2 = with vanillaPython.pkgs;
+    buildPythonPackage rec {
+      pname = "ghp-import2";
+      version = "1.0.1";
+      doCheck = false;
+      src = fetchPypi {
+         inherit pname version;
+         sha256 = "0zvdr67f1gr21ygnahmagfxbn1r1yb2gyiii9fb44nvcn935iygx";
+      };
+    };
+  python = vanillaPython.override {
+      packageOverrides = (self: super:
+         (mapAttrs (name: value: (
+         if (typeOf value) == "set" && value ? "overridePythonAttrs"
+         then
+           if name == "Nikola" then
+             (value.overridePythonAttrs (p: {
+                       propagatedBuildInputs = p.propagatedBuildInputs ++ [ super.wheel ghp_import2 ];
+                       doInstallCheck = false;
+                       doCheck = false; }))
+           else
+             (value.overridePythonAttrs (p: {
+                     doInstallCheck = false;
+                     doCheck = false;
+                     }))
+         else value))
+           super)
+      );
+    };
+in mkShell {
+    buildInputs = [ python.pkgs.Nikola  ];
 }
